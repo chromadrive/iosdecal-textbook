cfa68f9c387b7744cfa97d7a965222629cd51afa
chromadrive <chromadrive.prime@gmail.com>
2018-11-05 09:52:00 -0800

    Move to subdomain

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,15 +24,15 @@ const users = [
 const siteConfig = {
   title: 'iOS Development DeCal // Textbook', // Title for your website.
   tagline: 'iOS Development DeCal Textbook',
-  url: 'http://chromadrive.net/', // Your website URL
-  baseUrl: '/textbook/', // Base URL for your project */
+  url: 'http://textbook.chromadrive.net/', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'iosdecal-textbook',
-  organizationName: 'iosdecal',
+  organizationName: 'chromadrive',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
